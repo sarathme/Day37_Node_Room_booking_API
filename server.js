9fb325b91de1fc4e088dc746e4d751c70e298490
Sarasraman <sarathsatheesh603@gmail.com>
2024-09-04 19:46:58 +0530

    Initialize task with some folder structure

---
+++ server.js
@@ -0,0 +1,11 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = require("./app");
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
