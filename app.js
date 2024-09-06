6c0529b2016a3da20d52f40b8704732f45e9e9c7
Sarasraman <sarathsatheesh603@gmail.com>
2024-09-06 08:08:11 +0530

    Add routes for create booking and route for listing all rooms with bookings

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const roomRouter = require("./routes/roomRoutes");
+const customerRouter = require("./routes/customerRoutes");
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(express.json());
 
 // ROUTES
 app.use("/api/v1/rooms", roomRouter);
+app.use("/api/v1/customers", customerRouter);
 
 module.exports = app;
--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,12 +1,27 @@
 const express = require("express");
 
-const roomController = require("../controllers/roomController");
+// Importing the controller for rooms.
 
+const roomController = require("../controllers/roomsController");
+
+// Generating a router instance of express Router class
 const router = express.Router();
+
+// Assigning HTTP methods for root route.
 
 router
   .route("/")
   .get(roomController.getAllRooms)
   .post(roomController.createRoom);
+
+// Assigning HTTP methods for /bookings route.
+
+router.route("/bookings").get(roomController.getAllBookings);
+
+// Assigning HTTP for dynamic route with room id as endpoint.
+
+router.route("/:roomId").post(roomController.bookRoom);
+
+// Exporting the router instance.
 
 module.exports = router;
